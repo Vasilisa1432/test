0516527060572f86568b4fd8aa943a4a35c27877
Эминова Василиса <vasilisaeminova956@gmail.com>
2023-10-08 22:56:13 +0300

    Initial commit

---
+++ hello.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var time:=ReadInteger('Текущий час:');
+  case time of
+    4..10: WriteLn('Доброе утро, мир!');
+    11..16: WriteLn('Добрый день, мир!');
+    17..22: WriteLn('Добрый вечер, мир!');
+    else
+  end;
+end.
