1f881985157fc65b370e1c86d2afaef67e8e28b8
Vasilisa1432 <146511653+Vasilisa1432@users.noreply.github.com>
2023-10-08 23:40:20 +0300

    Add files via upload
    important fix

--- hello.pas
+++ hello.pas
@@ -1,9 +1,15 @@
 ﻿begin
-  var time:=ReadInteger('Текущий час:');
-  case time of
-    4..10: WriteLn('Доброе утро, мир!');
-    11..16: WriteLn('Добрый день, мир!');
-    17..22: WriteLn('Добрый вечер, мир!');
-    else
+  PrintLn('Choose your language: 1) Russian 2) English 3) Turkish');
+  var language := ReadInteger('Введите язык:');
+  Assert((language = 1) or (language = 2) or (language = 3));
+  case language of
+    1: PrintLn('Привет!');
+    2: PrintLn('Hello!');
+    3: PrintLn('Merhaba!');
   end;
 end.
+{
+Choose your language: 1) Russian 2) English 3) Turkish 
+Введите язык: 3
+Merhaba! 
+}
